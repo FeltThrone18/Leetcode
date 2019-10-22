6bd43ce253b6c3a09069385bec1ea146f3c6d099
Rahul Tiwari <32143478+FeltThrone18@users.noreply.github.com>
2019-10-22 16:02:19 -0700

    Update 34_FirstandLastOccurence.py

--- Medium/34_FirstandLastOccurence.py
+++ Medium/34_FirstandLastOccurence.py
@@ -1,1 +1,17 @@
+class solution:
+    
+    def searchRange(nums, target):
+        for i in range(len(nums)):
+            if nums[i] == target:
+                left_idx = i
+                break
+        else:
+            return [-1, -1]
+
+        for j in range(len(nums)-1,-1,-1):
+            if nums[j] == target:
+                right_idx = j
+                break
+
+        return [left_idx,right_idx]
 
